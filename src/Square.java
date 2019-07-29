6756d66f95e34d820a328d413a386a2e0c917f10
jstack123 <jstack3224@gmail.com>
2019-07-28 17:30:01 -0700

    caslting in progress

--- src/Square.java
+++ src/Square.java
@@ -6,19 +6,27 @@ public class Square extends JButton {
 	private boolean inMoveSet;
 	private Board b;
 	private Position pos;
-
-	public Square(Board b, Position pos) {
+	private King king;
+	boolean bool; 
+	public Square(Board b, Position pos, King king) {
 		this.inMoveSet = false;
 		this.b = b;
 		this.pos = pos;
+		this.king = king;
 	}
-
 	public void click() {
 		if (b.getSelectedPiece() == null && b.getPieceAtPos(pos) != null)
 			b.getPieceAtPos(pos).select();
-		else if (inMoveSet && b.getSelectedPiece() != null)
+		else if (inMoveSet && b.getSelectedPiece() != null) {
+			 bool = false;
+			if(b.getSelectedPiece().equals(king) && king.whiteAbleToCastle() && b.getPieceAtPos(new Position(7,4)).equals(king)) {
+				bool = true;
+			}
 			b.getSelectedPiece().move(pos);
-		else {
+			if(bool && b.getPieceAtPos(new Position(7,4)).equals(king)) {
+				
+			}
+		} else {
 			b.setSelectedPiece(null);
 			b.unhighlightMoves();
 		}
