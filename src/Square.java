9f92f75cc00bca747eea27f771aac1a994575a4d
jstack123 <jstack3224@gmail.com>
2019-07-31 16:31:06 -0700

    castling in progress

--- src/Square.java
+++ src/Square.java
@@ -7,25 +7,31 @@ public class Square extends JButton {
 	private Board b;
 	private Position pos;
 	private King king;
+	private Rook rook1;
+	private Rook rook2;
 	boolean bool; 
-	public Square(Board b, Position pos, King king) {
+	public Square(Board b, Position pos) {
 		this.inMoveSet = false;
 		this.b = b;
 		this.pos = pos;
-		this.king = king;
+//		this.king = king;
+//		this.rook1=rook1;
+//		this.rook2=rook2;
 	}
 	public void click() {
 		if (b.getSelectedPiece() == null && b.getPieceAtPos(pos) != null)
 			b.getPieceAtPos(pos).select();
 		else if (inMoveSet && b.getSelectedPiece() != null) {
-			 bool = false;
-			if(b.getSelectedPiece().equals(king) && king.whiteAbleToCastle() && b.getPieceAtPos(new Position(7,4)).equals(king)) {
-				bool = true;
-			}
+//			 bool = false;
+//			 System.out.println("" + bool);
+//			if(b.getSelectedPiece() != null && king.whiteAbleToCastle() == true) {
+//				bool = true;
+//				System.out.println("" + bool);
+//			}
 			b.getSelectedPiece().move(pos);
-			if(bool && b.getPieceAtPos(new Position(7,4)).equals(king)) {
-				
-			}
+//			if(bool && b.getPieceAtPos(new Position(7,2))!= null) {
+//				rook1.move(new Position(7,3));
+//			}
 		} else {
 			b.setSelectedPiece(null);
 			b.unhighlightMoves();
